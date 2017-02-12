724ad50fb2f6f7476d7a88ad2096198f0f2f8e70
Carl Janzen <carl.janzen@gmail.com>
2017-02-12 14:36:37 -0800

    add informative output to the entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-# change current user id to match the /code directory
-userid=`stat -c %u /code`
+echo "=== Attempting to create user to match owner of volume mounted at /tmp"
+userid=`stat -c %u /tmp`
 
-# exit if user is root
+echo "=== Executing as root because /tmp is owned by root."
 [[ $userid = 0 ]] && exec "$@"
 
-# exit if user already exists
+echo "=== Executing command as root because uid for mounted volume already exists."
 getent passwd ${userid} &&  exec "$@"
 
+echo "=== Adding user 'someuser' with uid ${userid} to match owner of /tmp"
 adduser --shell /bin/bash --uid ${userid} --disabled-password --gecos '' someuser
 export HOME=/home/someuser
 export SHELL=/bin/bash
 chown -R someuser:someuser /opt/conda
+echo "=== Passing control to 'someuser' with uid ${userid} to run: $@"
 exec gosu someuser "$@"
--- login
+++ login
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export HOME=/home/someuser
 export SHELL=/bin/bash
-[ -z "$@" ] && gosu someuser bash
-gosu someuser "$@"
+[ -z "$@" ] && gosu someuser bash ; exit 0
+gosu someuser "$@" ; exit 0
